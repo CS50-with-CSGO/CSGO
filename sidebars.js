3807e0f448f1de9ebeb0e5aebf15453d070c94f4
caesiumy <mn0658@naver.com>
2020-07-12 22:51:37 +0900

    docs: Add 정보의표현

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     "시작하기 전에": ["intro"],
-    "1. 컴퓨팅 사고": ["1/2진법"],
+    "1. 컴퓨팅 사고": ["1/2진법", "1/정보의표현"],
 
     etc: ["etc/doc1"],
     // etc: ["etc/doc1", "etc/doc2", "etc/doc3", "etc/mdx", "etc/test"],
